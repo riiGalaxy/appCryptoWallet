3c6240a6107b3c60f22c2e8378b01b59e67a35e3
riiGalaxy <riimolpeceres@gmail.com>
2022-03-10 17:54:59 +0100

    more stetic changes

--- src/components/ResumeProfile/ResumeProfile.jsx
+++ src/components/ResumeProfile/ResumeProfile.jsx
@@ -45,123 +45,119 @@ const ResumeProfile = ({ stats }) => {
   }, []);
 
   return (
-    <Grid columns={2} gap={10}>
-      <Link to={"/portfolio"}>
-        <p>Number Of Assets</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                <h1>{stats.data.length}</h1>
-              </Space>
+    <>
+      <Card block>
+        <Grid columns={2} className="resume-profit">
+          <div className="resume-profit-p">
+            <div className="container-profit">
+              <p className="parrafoStart">Total Profit/Loss</p>
             </div>
-          </Grid>
-        </Card>
-      </Link>
-
-      <Link to={"/portfolio"}>
-        <p>Amount Invested ($)</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <div>
-                <h1>{totalCalc}</h1>
+          </div>
+          <div className="container-profit">
+            <h1>{totalProfit}</h1>
+            <br></br>
+            <h1>{totalProfitPercent}%</h1>
+          </div>
+        </Grid>
+      </Card>
+      <Grid columns={2} gap={10} className="gridParrafo">
+        <Link to={"/portfolio"}>
+          <p>Number Of Assets</p>
+          <Card block>
+            <Grid className="homeList" columns={1} gap={15}>
+              <div className="container-card-investment">
+                <Space wrap>
+                  <h1>{stats.data.length}</h1>
+                </Space>
               </div>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
+            </Grid>
+          </Card>
+        </Link>
 
-      <Link to={"/portfolio"}>
-        <p>Total Profit/Loss ($)</p>
-        <Card block>
-          <Grid className="homeList" columns={2} gap={15}>
-            <div className="container-card">
-              <div>
-                <h1>{totalProfit}</h1>
+        <Link to={"/portfolio"}>
+          <p>Invested Money</p>
+          <Card block>
+            <Grid className="homeList" columns={1} gap={15}>
+              <div className="container-card-investment">
+                <div>
+                  <h1>{totalCalc} $</h1>
+                </div>
               </div>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
+            </Grid>
+          </Card>
+        </Link>
 
-      <Link to={"/portfolio"}>
-        <p>Total profit/loss (%)</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                <h1>{totalProfitPercent}%</h1>
-              </Space>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
+        {/* <Link to={"/portfolio"}>
+          <p>Total Profit/Loss ($)</p>
+          <Card block>
+            <Grid className="homeList" columns={2} gap={15}>
+              <div className="container-card">
+                <div>
+                  <h1>{totalProfit}</h1>
+                </div>
+              </div>
+            </Grid>
+          </Card>
+        </Link>
 
-      <Link to={"/portfolio"}>
-        <p>Best invested</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                {compareInvestment.length > 0 && (
-                  <h1>{compareInvestment[0].value} $</h1>
-                )}
-              </Space>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
+        <Link to={"/portfolio"}>
+          <p>Total profit/loss (%)</p>
+          <Card block>
+            <Grid className="homeList" columns={1} gap={15}>
+              <div className="container-card-investment">
+                <Space wrap>
+                  <h1>{totalProfitPercent}%</h1>
+                </Space>
+              </div>
+            </Grid>
+          </Card>
+        </Link> */}
 
-      <Link to={"/portfolio"}>
-        <p>Worst invested</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                {compareInvestment.length > 0 && (
-                  <h1>
-                    {compareInvestment[compareInvestment.length - 1].value} $
-                  </h1>
-                )}
-              </Space>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
+        <Link to={"/portfolio"}>
+          <p>Best Performance</p>
+          <Card block>
+            <Grid className="homeList" columns={1} gap={15}>
+              <div className="container-card-investment">
+                <Space wrap>
+                  {compareInvestment.length > 0 && (
+                    <h1>{compareInvestment[0].coin}</h1>
+                  )}
+                </Space>
+                <Space wrap>
+                  {compareInvestment.length > 0 && (
+                    <h1 className="green">{compareInvestment[0].value} $</h1>
+                  )}
+                </Space>
+              </div>
+            </Grid>
+          </Card>
+        </Link>
 
-      <Link to={"/portfolio"}>
-        <p>Best invested</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                {compareInvestment.length > 0 && (
-                  <h1>{compareInvestment[0].coin}</h1>
-                )}
-              </Space>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
-
-      <Link to={"/portfolio"}>
-        <p>Worst invested</p>
-        <Card block>
-          <Grid className="homeList" columns={1} gap={15}>
-            <div className="container-card-investment">
-              <Space wrap>
-                {compareInvestment.length > 0 && (
-                  <h1>
-                    {compareInvestment[compareInvestment.length - 1].coin}
-                  </h1>
-                )}
-              </Space>
-            </div>
-          </Grid>
-        </Card>
-      </Link>
-    </Grid>
+        <Link to={"/portfolio"}>
+          <p>Worst Performance</p>
+          <Card block>
+            <Grid className="homeList" columns={1} gap={15}>
+              <div className="container-card-investment">
+                <Space wrap>
+                  {compareInvestment.length > 0 && (
+                    <h1>
+                      {compareInvestment[compareInvestment.length - 1].coin}
+                    </h1>
+                  )}
+                </Space>
+                <Space wrap>
+                  {compareInvestment.length > 0 && (
+                    <h1 className="red">
+                      {compareInvestment[compareInvestment.length - 1].value} $
+                    </h1>
+                  )}
+                </Space>
+              </div>
+            </Grid>
+          </Card>
+        </Link>
+      </Grid>
+    </>
   );
 };
 
